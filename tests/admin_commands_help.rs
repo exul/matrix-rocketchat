fa892870551c471a620f553eb5642d76d0b5fe09
Andreas Brönnimann <foss@exul.org>
2017-02-03 22:16:39 +0100

    Add help command

---
+++ tests/admin_commands_help.rs
@@ -0,0 +1,50 @@
+#![feature(try_from)]
+
+extern crate matrix_rocketchat_test;
+extern crate router;
+extern crate ruma_client_api;
+extern crate ruma_identifiers;
+
+use std::convert::TryFrom;
+
+use matrix_rocketchat_test::{MessageForwarder, Test, default_timeout, helpers};
+use router::Router;
+use ruma_client_api::Endpoint;
+use ruma_client_api::r0::send::send_message_event::Endpoint as SendMessageEventEndpoint;
+use ruma_identifiers::{RoomId, UserId};
+
+
+#[test]
+fn help_command_when_not_connected() {
+    let (message_forwarder, receiver) = MessageForwarder::new();
+    let mut matrix_router = Router::new();
+    matrix_router.put(SendMessageEventEndpoint::router_path(), message_forwarder, "send_message_event");
+    let test = Test::new().with_matrix_routes(matrix_router).with_rocketchat_mock().with_admin_room().run();
+
+    helpers::send_room_message_from_matrix(&test.config.as_url,
+                                           RoomId::try_from("!admin:localhost").unwrap(),
+                                           UserId::try_from("@spec_user:localhost").unwrap(),
+                                           "help".to_string());
+
+    let message_received_by_matrix = receiver.recv_timeout(default_timeout()).unwrap();
+    assert!(message_received_by_matrix.contains("You have to connect this room to a Rocket.Chat server."));
+}
+
+#[test]
+fn help_command_when_connected() {
+    let (message_forwarder, receiver) = MessageForwarder::new();
+    let mut matrix_router = Router::new();
+    matrix_router.put(SendMessageEventEndpoint::router_path(), message_forwarder, "send_message_event");
+    let test = Test::new().with_matrix_routes(matrix_router).with_rocketchat_mock().with_connected_admin_room().run();
+
+    helpers::send_room_message_from_matrix(&test.config.as_url,
+                                           RoomId::try_from("!admin:localhost").unwrap(),
+                                           UserId::try_from("@spec_user:localhost").unwrap(),
+                                           "help".to_string());
+
+    // discard welcome message
+    receiver.recv_timeout(default_timeout()).unwrap();
+
+    let message_received_by_matrix = receiver.recv_timeout(default_timeout()).unwrap();
+    assert!(message_received_by_matrix.contains("You have to login before you can use the application service, there are two ways to do that"));
+}

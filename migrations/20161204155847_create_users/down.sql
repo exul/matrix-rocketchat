8eaa58836445bd0638f699b540dcbb51fb93dee4
Andreas Brönnimann <foss@exul.org>
2017-10-13 20:47:06 +0200

    Remove unused migrations

--- migrations/20161204155847_create_users/down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE matrix_users;
--- migrations/20161204173455_create_users_in_rooms/up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users_in_rooms(
-  matrix_user_id VARCHAR NOT NULL,
-  matrix_room_id VARCHAR NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY(matrix_user_id) REFERENCES users(matrix_user_id),
-  FOREIGN KEY(matrix_room_id) REFERENCES rooms(matrix_room_id),
-  CONSTRAINT users_in_rooms_pk PRIMARY KEY (matrix_user_id, matrix_room_id)
-)

2c1dc3812a72fce4fb84bee973f901d2039f9330
Andreas Brönnimann <foss@exul.org>
2017-10-25 17:06:42 +0200

    Only create the virtual user when it's actually needed

--- src/matrix-rocketchat/handlers/rocketchat/forwarder.rs
+++ src/matrix-rocketchat/handlers/rocketchat/forwarder.rs
@@ -2,7 +2,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 use diesel::sqlite::SqliteConnection;
 use slog::Logger;
-use ruma_identifiers::{RoomId, UserId};
+use ruma_identifiers::RoomId;
 
 use i18n::*;
 use api::{MatrixApi, RocketchatApi};
@@ -33,9 +33,6 @@ pub struct Forwarder<'a> {
 impl<'a> Forwarder<'a> {
     /// Send a message to the Matrix channel.
     pub fn send(&self, server: &RocketchatServer, message: &Message) -> Result<()> {
-        let sender_id =
-            self.virtual_user_handler.find_or_register(server.id.clone(), message.user_id.clone(), message.user_name.clone())?;
-
         if !self.is_sendable_message(message.user_id.clone(), server.id.clone())? {
             debug!(
                 self.logger,
@@ -44,7 +41,7 @@ impl<'a> Forwarder<'a> {
             return Ok(());
         }
 
-        let matrix_room_id = match self.prepare_room(server, sender_id.clone(), message)? {
+        let matrix_room_id = match self.prepare_room(server, message)? {
             Some(matrix_room_id) => matrix_room_id,
             None => {
                 debug!(
@@ -56,6 +53,8 @@ impl<'a> Forwarder<'a> {
             }
         };
 
+        let sender_id =
+            self.virtual_user_handler.find_or_register(server.id.clone(), message.user_id.clone(), message.user_name.clone())?;
         let current_displayname = self.matrix_api.get_display_name(sender_id.clone())?.unwrap_or_default();
         if message.user_name != current_displayname {
             debug!(self.logger, "Display name changed from `{}` to `{}`, will update", current_displayname, message.user_name);
@@ -80,22 +79,22 @@ impl<'a> Forwarder<'a> {
         }
     }
 
-    fn prepare_room(&self, server: &RocketchatServer, sender_id: UserId, message: &Message) -> Result<Option<RoomId>> {
+    fn prepare_room(&self, server: &RocketchatServer, message: &Message) -> Result<Option<RoomId>> {
         let is_direct_message_room = message.channel_id.contains(&message.user_id);
         if is_direct_message_room {
-            self.prepare_dm_room(server, sender_id, message)
+            self.prepare_dm_room(server, message)
         } else {
-            self.prepare_room_for_channel(server, sender_id, message)
+            self.prepare_room_for_channel(server, message)
         }
     }
 
-    fn prepare_dm_room(&self, server: &RocketchatServer, sender_id: UserId, message: &Message) -> Result<Option<RoomId>> {
+    fn prepare_dm_room(&self, server: &RocketchatServer, message: &Message) -> Result<Option<RoomId>> {
         let matrix_room_id = match self.try_to_find_or_create_direct_message_room(server, message)? {
             Some(matrix_room_id) => matrix_room_id,
             None => return Ok(None),
         };
 
-        self.invite_user_into_direct_message_room(server, sender_id, matrix_room_id.clone(), message)?;
+        self.invite_user_into_direct_message_room(server, matrix_room_id.clone(), message)?;
         Ok(Some(matrix_room_id))
     }
 
@@ -112,7 +111,6 @@ impl<'a> Forwarder<'a> {
     fn invite_user_into_direct_message_room(
         &self,
         server: &RocketchatServer,
-        matrix_user_id: UserId,
         matrix_room_id: RoomId,
         message: &Message,
     ) -> Result<()> {
@@ -121,7 +119,7 @@ impl<'a> Forwarder<'a> {
             match self.find_matching_user_for_direct_message(server, message)? {
                 Some(other_user) => {
                     let invited_user_id = other_user.matrix_user_id.clone();
-                    let inviting_user_id = matrix_user_id.clone();
+                    let inviting_user_id = self.matrix_api.get_room_creator(matrix_room_id.clone())?;
                     self.virtual_user_handler.add_to_room(invited_user_id.clone(), inviting_user_id, matrix_room_id)?;
                 }
                 None => {
@@ -133,12 +131,7 @@ impl<'a> Forwarder<'a> {
         Ok(())
     }
 
-    fn prepare_room_for_channel(
-        &self,
-        server: &RocketchatServer,
-        sender_id: UserId,
-        message: &Message,
-    ) -> Result<Option<RoomId>> {
+    fn prepare_room_for_channel(&self, server: &RocketchatServer, message: &Message) -> Result<Option<RoomId>> {
         let matrix_room_id =
             match Room::matrix_id_from_rocketchat_channel_id(self.config, self.matrix_api, &server.id, &message.channel_id)? {
                 Some(matrix_room_id) => matrix_room_id,
@@ -146,6 +139,8 @@ impl<'a> Forwarder<'a> {
             };
 
         let inviting_user_id = self.config.matrix_bot_user_id()?;
+        let sender_id =
+            self.virtual_user_handler.find_or_register(server.id.clone(), message.user_id.clone(), message.user_name.clone())?;
         self.virtual_user_handler.add_to_room(sender_id, inviting_user_id, matrix_room_id.clone())?;
 
         Ok(Some(matrix_room_id))
